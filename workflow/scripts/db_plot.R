5d4ad52342a9dc4f41133a632ef8e1af57f0cd5c
Caspar Gross <mail@caspar.one>
2022-09-05 16:13:35 +0200

    Fix project colours in plots

--- workflow/scripts/db_plot.R
+++ workflow/scripts/db_plot.R
@@ -5,17 +5,70 @@ suppressPackageStartupMessages(library(purrr))
 suppressPackageStartupMessages(library(rjson))
 suppressPackageStartupMessages(library(ggplot2))
 
-#print(str(snakemake))
-#print(snakemake)
+# Define default in/out files for testing if not run inside snakemake
+if (exists("snakemake")) {
+  input_csv <- snakemake@input$csv
+  out_device_pos <- snakemake@output$device_pos
+  out_len_yield <- snakemake@output$len_yield
+  out_timeline <- snakemake@output$timeline
+} else {
+  input_csv <- "table_out.csv"
+  out_device_pos <- "plot_device_pos.png"
+  out_len_yield <- "plot_len_yield.png"
+  out_timeline <- "plot_timeline.png"
+}
+
+
+# Setup plot theme
+my_palette <- function(...) {  
+    # Colour Brewer Set1
+    colours <- c(
+        "#E41A1C",
+        "#377EB8",
+        "#4DAF4A",
+        "#984EA3",
+        "#FF7F00",
+        "#FFFF33",
+        "#A65628",
+        "#F781BF",
+        "#999999"
+        )
+        
+    function(n) {
+        if (n == 0) {
+            stop("Must request at least one colour from a hue palette.", 
+                call. = FALSE)
+        } else if (n<10) {
+            sel_colours = colours[1:n]
+        } else {
+            sel_colours = colorRampPalette(colours)(n)
+        }
+        sel_colours
+    }
+    
+}
+
+scale_fill_custom <- function(..., aesthetics = "fill") {
+    
+    discrete_scale(aesthetics, "custom", my_palette(), ...)
+}
 
 # Load plot theme
-snakemake@source("theme_cas.R")
+mytheme <- function(){
+    # Change main theme
+    theme_bw() %+replace% 
+    theme(
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank()
+    ) 
+}
 
 # Load data
-dt <- read_csv(snakemake@input$csv, col_types = cols())
+dt <- read_csv(input_csv, col_types = cols())
 dtt <- dt %>%
   mutate(start_time = lubridate::as_datetime(start_time)) %>%
-  mutate(run_duration = lubridate::dhours(run_duration))
+  mutate(run_duration = lubridate::dhours(run_duration)) %>%
+  mutate(project = as.factor(project))
 
 # _______________________
 # Runs/Yield per week
@@ -25,9 +78,11 @@ dttw <- dtt %>%
 p_weeks <- ggplot(dttw, aes(x = week, fill = project)) +
   geom_histogram(col = "grey20", binwidth = as.numeric(lubridate::dweeks(x=1))) +
   labs(x = "", y = "Flowcells", fill = "Project") +
-  mytheme
+#  scale_x_date(date_minor_breaks = "1 week") +
+  mytheme() +
+  scale_fill_custom()
 ggsave(
-  snakemake@output$timeline, 
+  out_timeline,
   plot = p_weeks, 
   height = 5, 
   width = 8)
@@ -36,12 +91,13 @@ ggsave(
 # Scatterplot length/yield
 p_len_yield <- ggplot(dttw, aes(x = N50, y = bases_number, fill = project)) +
   geom_point(shape=21, col = "grey20", alpha = 0.9) + 
-  mytheme +
+  mytheme() +
+  scale_fill_custom() +
   labs(
     x = "Read length (N50)",
     y = "Sequencing yield (bases)")
 ggsave(
-  snakemake@output$len_yield, 
+  out_len_yield, 
   plot=p_len_yield, 
   height = 5, 
   width = 8)
@@ -75,9 +131,9 @@ p_position <-
     geom_tile() +
     coord_fixed(ratio=1) +
     facet_grid(cols = vars(key)) + 
-    mytheme 
+    mytheme()
 ggsave(
-  snakemake@output$device_pos, 
+  out_device_pos, 
   plot = p_position, 
   height = 5, 
   width = 8)
