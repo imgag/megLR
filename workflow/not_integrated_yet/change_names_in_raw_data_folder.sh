5f1032bc895abc7b7f5cf84b456ab72a2910fa42
Caspar Gross <mail@caspar.one>
2023-02-03 11:18:19 +0100

    Add script to change sample and project names in all files in a raw data folder

---
+++ workflow/not_integrated_yet/change_names_in_raw_data_folder.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Change samplename for all files in a raw data folder (excluding .fast5)
+
+old_sample=21073LRa141_02787  # Give full old samplename!
+new_sample=21102LRa041_02782  # Full new samplename
+
+old_project=21073
+new_project=21102
+
+find . -type f -not -name '*.fast5' -not -name '*.fastq.gz' -exec sed -i "s/$old_sample/$new_sample/g" {} +
+find . -type f -not -name '*.fast5' -not -name '*.fastq.gz' -exec sed -i "s/$old_project/$new_project/g" {} +
+
+change_fastq () {
+    tmp=$(mktemp)
+    echo "Modifying file $1 in $tmp "
+    zcat $1 > $tmp
+    sed -i "s/$old_sample/$new_sample/g" $tmp
+    sed -i "s/$old_project/$new_project/g" $tmp
+    gzip -c $tmp > $1
+}
+
+export old_sample
+export new_sample
+export old_project
+export new_project
+export -f change_fastq
+
+find . -type f -name '*.fastq.gz' -exec bash -c 'change_fastq "{}"' \;
