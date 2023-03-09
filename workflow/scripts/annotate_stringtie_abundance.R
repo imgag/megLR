c1d52f2869eb65f3d528d2ae08f6148a0316c351
Caspar Gross <mail@caspar.one>
2023-03-09 18:23:37 +0100

    Add script to merge stringtie and sqanti results

--- workflow/scripts/annotate_stringtie_abundance.R
+++ workflow/scripts/annotate_stringtie_abundance.R
@@ -1,19 +1,49 @@
 #!/bin/env Rscript
 
 suppressPackageStartupMessages(library(data.table))
-suppressPackageStartupMessages(library(purrr))
-suppressPackageStartupMessages(library(rjson))
-suppressPackageStartupMessages(library(ggplot2))
+suppressPackageStartupMessages(library(annotables))
 
 # Define default in/out files for testing if not run inside snakemake
 if (exists("snakemake")) {
-  input_csv <- snakemake@input$csv
-  out_device_pos <- snakemake@output$device_pos
-  out_len_yield <- snakemake@output$len_yield
-  out_timeline <- snakemake@output$timeline
-} else {
-  input_csv <- "table_out.csv"
-  out_device_pos <- "plot_device_pos.png"
-  out_len_yield <- "plot_len_yield.png"
-  out_timeline <- "plot_timeline.png"
+  abund <- snakemake@input$abund
+  classif <- snakemake@input$classif
+  out_tsv <- snakemake@output$tsv
+  build <- snakemake@params$build
 }
+
+dta <- fread(abund)
+dta <- dta[Coverage > 0,,]
+dtc <- fread(classif)
+
+dtc[,c("STRG_Gene_ID","STRG+Isoform_ID") := tstrsplit(isoform, "[.]", keep=c(2,3))]
+dtc[,STRG_gene := paste0("STRG.",STRG_Gene_ID)]
+
+dt <- merge(dta, dtc, by.x = "Gene ID", by.y =  "STRG_gene")
+dt <- dt[,.(
+  "chr" = Reference,
+  "start" = Start,
+  "end" = End,
+  "strand" = Strand,
+  "strg_isoform" = isoform,
+  associated_gene,
+  associated_transcript, 
+  "coverage" = Coverage,
+  TPM, FPKM, 
+  length, exons, ref_length, ref_exons,
+  structural_category,
+  subcategory, 
+  coding,
+  ORF_seq
+)]
+dt[,ensid:=tstrsplit(associated_gene, '[.]', keep=1)]
+
+dtm <- merge(
+  dt, 
+  as.data.table(grch38)[,.(ensgene, symbol, description),],
+  by.x="ensid",
+  by.y="ensgene",
+  all.x=TRUE)
+dtm <- dtm[,!"ensid",]
+dtm <- dtm[order(chr, start)]
+
+fwrite(dtm, out_tsv, sep="\t")
